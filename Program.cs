ab6dff85836b5de5516ecd6150af8d9a7a6f0ea8
Anmol Thapa <anmolthapa@Anmols-MacBook-Air.local>
2026-04-20 09:22:58 +0545

    First Program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.WriteLine("Hello, World!");
+// for (int i= 0; i<10; i++)
+// {
+//  Console.WriteLine(i); 
+// }
+
+// int[]numbers= [1,2,3,4,5];
+// foreach(int number in numbers)
+// {
+//   Console.WriteLine(number);  
+// }
+
+
+using System.Transactions;
+
+Console.WriteLine("enter your name");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
